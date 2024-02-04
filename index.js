07df2345088392ef14b48a64d6eda944ef725bc4
silas <silasgebhart12@gmail.com>
2024-02-04 13:09:59 -0600

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100);
+
+let num3 = 24;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+let max = 20;
